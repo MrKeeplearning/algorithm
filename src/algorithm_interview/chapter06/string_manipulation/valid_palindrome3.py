5a6e1efa659ab560b9ca38bc73e0237efd6fb6e4
MrKeeplearning <jaeseok1115@gmail.com>
2022-05-23 23:15:24 +0900

    문자열 뒤집기 관련 문제 풀이 및 학습

--- src/algorithm_interview/chapter06/string_manipulation/valid_palindrome3.py
+++ src/algorithm_interview/chapter06/string_manipulation/valid_palindrome3.py
@@ -5,6 +5,7 @@ import re
 def is_palindrome(self, s: str) -> bool:
     s = s.lower()
     # 정규식으로 문자 필터링
+    # 소문자 알파벳과 0부터 9까지의 정수가 아닐 경우 ''로 치환한다.
     s = re.sub('[^a-z0-9]', '', s)
     # 슬라이싱
     return s == s[::-1]
