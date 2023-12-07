a8e0d1746dcdf87346cb39bf3186dcd8ba0c8c0b
MrKeeplearning <jaeseok1115@gmail.com>
2023-12-07 23:20:06 +0900

    programmers 120842 2차원으로 만들기 해결

--- src/main/java/programmers/beginners/lessons120842/README.md
+++ src/main/java/programmers/beginners/lessons120842/README.md
@@ -1,2 +1,13 @@
 ## [2차원으로 만들기](https://school.programmers.co.kr/learn/courses/30/lessons/120842)
 
+### Solution1
+
+```text
+테스트 1 〉	통과 (0.02ms, 75.5MB)
+테스트 2 〉	통과 (0.03ms, 75.3MB)
+테스트 3 〉	통과 (0.03ms, 80.1MB)
+테스트 4 〉	통과 (0.02ms, 77.6MB)
+테스트 5 〉	통과 (0.02ms, 77.4MB)
+테스트 6 〉	통과 (0.02ms, 70.6MB)
+테스트 7 〉	통과 (0.01ms, 78.6MB)
+```
--- src/main/java/programmers/beginners/lessons120842/Solution.java
+++ src/main/java/programmers/beginners/lessons120842/Solution.java
@@ -2,25 +2,16 @@ package programmers.beginners.lessons120842;
 
 public class Solution {
 
-    public static void main(String[] args) {
-        solution([1, 2, 3, 4, 5, 6, 7, 8],2)
-    }
-
     /**
      * num_list를 n으로 나누면 총 행의 개수가 나온다.
      */
-    public static int[][] solution(int[] num_list, int n) {
-
-        int len = num_list.length;
-        int rowLen = len / n;
-
-        int[][] answer = {};
-
-        int count = 0;
-        for (int i = 0; i < rowLen; i++) {
+    public int[][] solution(int[] num_list, int n) {
+        int row_length = num_list.length / n;
+        int[][] answer = new int[row_length][n];
+        int index = 0;  // num_list 값을 가져올 때 사용할 index
+        for (int i = 0; i < row_length; i++) {
             for (int j = 0; j < n; j++) {
-                answer[i][j] = num_list[count];
-                count++;
+                answer[i][j] = num_list[index++];
             }
         }
         return answer;
