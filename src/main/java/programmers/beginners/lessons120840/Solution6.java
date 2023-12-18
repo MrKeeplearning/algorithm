11c240a7d2db61e97482fb24e1c872cb904e9bca
MrKeeplearning <jaeseok1115@gmail.com>
2023-12-19 01:08:52 +0900

    programmers 120840 구슬을 나누는 경우의 수 다른 풀이

--- src/main/java/programmers/beginners/lessons120840/Solution6.java
+++ src/main/java/programmers/beginners/lessons120840/Solution6.java
@@ -1,22 +1,23 @@
 package programmers.beginners.lessons120840;
 
-public class Solution6 {
+class Solution6 {
+    /**
+     * 재귀를 사용한 풀이
+     */
+    static int answer = 0;
+
     public int solution(int balls, int share) {
-        int answer = 0;
+
+        share_count(balls, share, 0, 0);
         return answer;
     }
-
-//    static int combination(int balls, int temp, int start, int share) {
-//        int answer = 0;
-//        if (temp == share) {
-//            answer++;
-//            return;
-//        }
-//        for (int i = start; i < balls; i++) {
-//            temp++;
-//            combination(balls, temp, i + 1, share);
-//            temp--;
-//        }
-//        return answer;
-//    }
+    static void share_count(int balls, int share, int count, int start){
+        if(count == share) {
+            answer++;
+            return;
+        }
+        for(int i=start; i<balls; i++){
+            share_count(balls, share, count+1, i+1);
+        }
+    }
 }
