3d61e2132c9d87e72a60357fe5ed612d1a745ab0
MrKeeplearning <jaeseok1115@gmail.com>
2023-12-27 16:00:37 +0900

    programmers 87389 나머지가 1이 되는 수 찾기

---
+++ src/main/java/programmers/lv1/lessons87389/Solution.java
@@ -0,0 +1,14 @@
+package programmers.lv1.lessons87389;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for (int i = 1; i < n; i++) {
+            if (n % i == 1) {
+                answer = i;
+                break;
+            }
+        }
+        return answer;
+    }
+}
