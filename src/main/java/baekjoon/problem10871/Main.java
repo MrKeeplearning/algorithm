e6302696784fb478fb95da506728df4f2e6c6ff3
MrKeeplearning <fossilhunter@hanmail.net>
2024-05-14 18:07:46 +0900

    백준 10871 "X보다 작은 수"

---
+++ src/main/java/baekjoon/problem10871/Main.java
@@ -0,0 +1,27 @@
+package baekjoon.problem10871;
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int N, X;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        X = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        while(st.hasMoreTokens()) {
+            int num = Integer.parseInt(st.nextToken());
+            if (num < X) {
+                bw.write(num + " ");
+            }
+        }
+
+        br.close();
+        bw.close();
+    }
+}
