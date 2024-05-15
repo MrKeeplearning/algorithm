e1cedfabfad564c7a0a3165216cf20f38fb09668
MrKeeplearning <fossilhunter@hanmail.net>
2024-05-15 20:20:55 +0900

    백준 10988

---
+++ src/main/java/baekjoon/problem10988/Main.java
@@ -0,0 +1,31 @@
+package baekjoon.problem10988;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String word = br.readLine();
+        bw.write(isPalindrome(word) ? "1" : "0");
+        bw.flush();
+
+        br.close();
+        bw.close();
+    }
+
+    public static boolean isPalindrome(String s) {
+        int i = 0;
+        int j = s.length() - 1;
+
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
