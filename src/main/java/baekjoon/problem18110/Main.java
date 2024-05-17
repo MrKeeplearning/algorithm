9b89a76d44ddd5ba32681dc7706610fbc1f93b03
MrKeeplearning <fossilhunter@hanmail.net>
2024-05-17 23:42:53 +0900

    백준 18110

---
+++ src/main/java/baekjoon/problem18110/Main.java
@@ -0,0 +1,41 @@
+package baekjoon.problem18110;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());    // 난이도 의견의 개수 n
+
+        // 난이도 의견이 완전히 주어지지 않은 경우에는 절사평균을 계산할 수 없다.
+        if (n == 0) {
+            bw.write(String.valueOf(0));
+            bw.flush();
+            return;
+        }
+
+        int[] difficultyOpinions = new int[n];
+        int ignoreOpinions = (int) Math.round(n * 0.15);    // 절사평균을 적용했을 때 위아래에서 제거할 의견 수
+        int trimmedOpinions = n - 2 * ignoreOpinions;   // 위아래 15%를 제외한 의견의 수
+        double sum = 0.0;
+
+        for (int i = 0; i < n; i++) {
+            difficultyOpinions[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(difficultyOpinions);    // 모든 난이도 의견을 오름차순 정렬.
+
+        // 위아래 15%를 제외한 의견들을 가지고 절사평균을 계산한다.
+        for (int i = ignoreOpinions; i < n - ignoreOpinions; i++) {
+            sum += difficultyOpinions[i];
+        }
+
+        int trimmedMean = (int) Math.round(sum / trimmedOpinions);
+        bw.write(String.valueOf(trimmedMean));
+        bw.flush();
+
+        br.close();
+        bw.close();
+    }
+}
---
+++ src/main/java/baekjoon/problem18110/PriorityQueueSolution.java
@@ -0,0 +1,42 @@
+package baekjoon.problem18110;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * 우선순위 큐를 활용한 풀이
+ */
+public class PriorityQueueSolution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        if (n == 0) {
+            bw.write("0");
+            bw.flush();
+            return;
+        }
+
+        // 우선순위큐를 사용하면 가장 작은 값을 효율적으로 찾을 수 있음.
+        // 처음 푼 풀이 대비 모든 난이도 의견을 오름차순 정렬하는 비용을 줄일 수 있음.
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i = 0; i < n; i++) {
+            pq.offer(Integer.parseInt(br.readLine()));
+        }
+
+        int trimSize = (int) Math.round(n * 0.15);
+        double sum = 0;
+        for (int i = 0; i < n; i++) {
+            int num = pq.poll();
+            if (i < trimSize || i >= n - trimSize) continue;
+            sum += num;
+        }
+
+        bw.write(Math.round(sum / (n - 2 * trimSize)) + "\n");
+        bw.flush();
+
+        br.close();
+        bw.close();
+    }
+}
