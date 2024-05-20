7a2e67a1be5fa5da594195d0354ebd055359102f
MrKeeplearning <fossilhunter@hanmail.net>
2024-05-20 20:09:56 +0900

    백준 10157 실패(시간초과)

---
+++ src/main/java/baekjoon/problem1058/Main.java
@@ -0,0 +1,16 @@
+package baekjoon.problem1058;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+
+        br.close();
+        bw.close();
+    }
+}
