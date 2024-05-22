3032426e4c13b7dbb3c2f1249769359b9e239896
MrKeeplearning <fossilhunter@hanmail.net>
2024-05-22 20:20:07 +0900

    백준 15655 "N과 M(6)" -> DFS, 조합 공부 필요...

--- src/main/java/baekjoon/problem15655/Main.java
+++ src/main/java/baekjoon/problem15655/Main.java
@@ -1,36 +1,55 @@
 package baekjoon.problem15655;
 
 import java.io.*;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
+    static BufferedWriter bw;
+    static int N, M;
+    static int[] numbers, result;
+    static boolean[] check;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        check = new boolean[N];
+        numbers = new int[N];
+        result = new int[M];
+
         st = new StringTokenizer(br.readLine());
-        while (st.hasMoreTokens()) {
-            pq.offer(Integer.parseInt(st.nextToken()));
+        for (int i = 0; i < N; i++) {
+            numbers[i] = Integer.parseInt(st.nextToken());
         }
 
-        for (int i = 0; i < n; i++) {
-            int temp = 0;
-            for (int j = 0; j < i; j++) {
-                temp = pq.poll();
-            }
-            while (!pq.isEmpty()) {
-                bw.write(temp + " " + pq.poll());
-                bw.newLine();
-            }
-        }
+        Arrays.sort(numbers);
+
+        combination(0, 0);
 
         br.close();
         bw.close();
+    }
+
+    static void combination(int start, int depth) throws IOException {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                bw.write(result[i] + " ");
+            }
+            bw.newLine();
+            return;
+        }
+
+        for (int i = start; i < N; i++) {
+            if (!check[i]) {
+                check[i] = true;
+                result[depth] = numbers[i];
+                combination(i + 1, depth + 1);
+                check[i] = false;
+            }
+        }
     }
 }
