7dfb4585072e63f01b6538f1dab86acf3a96fef2
MrKeeplearning <fossilhunter@hanmail.net>
2024-05-31 19:29:31 +0900

    백준 18870 "좌표 압축"

---
+++ src/main/java/baekjoon/problem17626/Main.java
@@ -0,0 +1,25 @@
+package baekjoon.problem17626;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+//        int n = Integer.parseInt(br.readLine());
+
+        System.out.println(Math.sqrt(11339));
+        System.out.println(Math.pow(106, 2));
+        System.out.println(11339 - 11236);
+
+        System.out.println(Math.sqrt(103));
+        System.out.println(Math.pow(10, 2));
+
+        System.out.println(Math.sqrt(3));
+
+        br.close();
+        bw.close();
+    }
+}
