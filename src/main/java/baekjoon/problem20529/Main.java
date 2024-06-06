9eba396cd85b8b19918690880724b0e21f06b662
MrKeeplearning <fossilhunter@hanmail.net>
2024-06-06 11:34:56 +0900

    백준 15649 "N과 M (1)"

---
+++ src/main/java/baekjoon/problem20529/Main.java
@@ -0,0 +1,29 @@
+package baekjoon.problem20529;
+
+import java.io.*;
+
+public class Main {
+    static int N;
+    static String[] mbti;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(br.readLine());
+        
+
+        br.close();
+        bw.close();
+    }
+
+    public static int getDistance(String mbti1, String mbti2) {
+        int count = 0;
+        for (int i = 0; i < 4; i++) {
+            if (mbti1.charAt(i) != mbti2.charAt(i)) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
