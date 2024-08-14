699519ff3e8d1c22972f0837f46d73e630b86e6a
MrKeeplearning <jaeseok1115@gmail.com>
2024-08-14 21:55:57 +0900

    백준 5086 "배수와 약수"

---
+++ src/main/java/baekjoon/problem5086/Main.java
@@ -0,0 +1,38 @@
+package baekjoon.problem5086;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String answer;
+
+        while (true) {
+            String inputLine = br.readLine();
+            if (inputLine.equals("0 0")) break;
+
+            StringTokenizer st = new StringTokenizer(inputLine);
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            // 첫 번째 숫자가 두 번째 숫자의 약수
+            if (b % a == 0) {
+                answer = "factor";
+            }
+            // 첫 번째 숫자가 두 번째 숫자의 배수
+            else if (a % b == 0) {
+                answer = "multiple";
+            } else {
+                answer = "neither";
+            }
+
+            bw.write(answer + "\n");
+        }
+        bw.flush();
+
+        br.close();
+        bw.close();
+    }
+}
