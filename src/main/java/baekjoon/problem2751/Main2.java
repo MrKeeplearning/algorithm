2e8e9d391a5be73592dd46877ce28f9450f984dd
MrKeeplearning <fossilhunter@hanmail.net>
2024-08-30 12:17:28 +0900

    백준 2751 "수 정렬하기 2"

---
+++ src/main/java/baekjoon/problem2751/Main2.java
@@ -0,0 +1,27 @@
+package baekjoon.problem2751;
+
+import java.io.*;
+import java.util.*;
+
+public class Main2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        List<Integer> numbers = new ArrayList<>(N);
+
+        for (int i = 0; i < N; i++) {
+            numbers.add(Integer.parseInt(br.readLine()));
+        }
+
+        Collections.sort(numbers);
+
+        for (int num : numbers) {
+            bw.write(num + "\n");
+        }
+
+        br.close();
+        bw.close();
+    }
+}
