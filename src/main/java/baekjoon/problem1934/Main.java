d8c340adb28ebb35dad941452e73630911d735df
MrKeeplearning <fossilhunter@hanmail.net>
2024-09-07 17:42:10 +0900

    백준 1934 "최소공배수"

---
+++ src/main/java/baekjoon/problem1934/Main.java
@@ -0,0 +1,38 @@
+package baekjoon.problem1934;
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            bw.write(lcm(A, B) + "\n");
+        }
+
+        br.close();
+        bw.close();
+    }
+
+    public static int euclidGCD(int e1, int e2) {
+        int a = Math.max(e1, e2);
+        int b = Math.min(e1, e2);
+
+        if (b == 0) {
+            return a;
+        }
+        return euclidGCD(b, a % b);
+    }
+
+    public static int lcm(int l1, int l2) {
+        int gcd = euclidGCD(l1, l2);
+        return Math.abs(l1 * l2) / gcd;
+    }
+}
