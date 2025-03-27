32c67e85902720c6ed88fe9dd2afa691d198a5fc
MrKeeplearning <fossilhunter@hanmail.net>
2025-03-27 23:41:25 +0900

    백준 11050: 이항계수1

---
+++ src/main/java/baekjoon/problem11050/Main.java
@@ -0,0 +1,28 @@
+package baekjoon.problem11050;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int answer = factorial(N) / (factorial(K) * factorial(N - K));
+        bw.write(answer + "\n");
+        bw.flush();
+
+        br.close();
+        bw.close();
+    }
+
+    public static int factorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        return n * factorial(n - 1);
+    }
+}
